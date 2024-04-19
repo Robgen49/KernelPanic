ee70bdbe5d7ff6f02ad0652098fcbfa11e806eff
Antares <antarespirat@gmail.com>
2024-04-19 19:23:24 +0300

    oauth

---
+++ BACK/Kernel-panic/src/main/java/com/oauth2/Kernel/panic/KernelPanicApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2.Kernel.panic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernelPanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KernelPanicApplication.class, args);
+	}
+
+}
---
+++ BACK/Kernel-panic/src/test/java/com/oauth2/Kernel/panic/KernelPanicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.Kernel.panic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernelPanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
