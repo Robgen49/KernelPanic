72b8e3aa253c9fcdcc453df8dd3e4022685ac3ff
Antares <antarespirat@gmail.com>
2024-04-20 23:20:25 +0300

    CORS

--- BACK/Kernel-panic/src/main/java/com/oauth2/Kernel/panic/config/SecurityConfig.java
+++ BACK/Kernel-panic/src/main/java/com/oauth2/Kernel/panic/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**","/swagger-ui/**","v3/api-docs","/swagger/**")
                         .permitAll()
                         .anyRequest()
